73e31cf5fd2fea377c0fab4074e9d45abad38e99
hqnghi88 <hqnghi88@gmail.com>
2022-04-26 10:13:19 +0700

    init example

---
+++ index.js
@@ -0,0 +1,33 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+const PORT = process.env.PORT || 80
+var app = express();
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname + '/examples/20-game.html'));
+});
+ 
+
+app.get('/home', function(request, response) {
+    if (request.session.loggedin) {
+        response.sendFile(__dirname + '/examples/20-game.html');
+    } else {
+        //response.send('Please login to view this page!');
+        response.redirect('/');
+        response.end();
+    }
+});
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
