6f5676120032265e4da571ec91270d24689dd813
hqnghi88 <hqnghi88@gmail.com>
2022-04-26 16:51:35 +0700

    s

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 app.use('/examples', express.static(path.join(__dirname, 'examples')))
+app.use('/models', express.static(path.join(__dirname, 'models')))
 
 
 app.get('/', function(request, response) {
