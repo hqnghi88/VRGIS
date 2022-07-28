92273b1dd8ec561e87ff9e5fb1b69398b5a3ef27
HUYNH Nghi <hqnghi88@gmail.com>
2022-07-28 21:25:34 +0700

    try version

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/home', function (request, response) {
 server.lastPlayderID = 0;
 
 server.listen(process.env.PORT || 80, function () {
-    console.log('Version '+HEROKU_RELEASE_VERSION);
+    console.log('Version '+process.env.HEROKU_RELEASE_VERSION);
     console.log('Listening on ' + server.address().port);
 });
 
